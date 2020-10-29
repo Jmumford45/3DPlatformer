b38f7f63f811302faf9c4636c6b2b0a668eee780
Jmumford45 <JMumford45@gmail.com>
2020-10-28 18:48:58 -0700

    commit

---
+++ Platformer(untitled)/Assets/Scripts/GameController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private int[][] level = new int[][]
+    {
+        new int[] {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        new int[] {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        new int[] {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        new int[] {1,3,0,0,0,0,0,0,0,0,3,3,3,0,0,0,0,0,1},
+        new int[] {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1},
+        new int[] {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1},
+        new int[] {1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1},
+        new int[] {1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        new int[] {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        new int[] {1,0,0,0,0,0,3,0,3,0,0,0,0,0,0,0,0,0,1},
+        new int[] {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        new int[] {1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1},
+        new int[] {1,1,1,1,1,1,1,0,0,0,1,1,1,1,0,0,0,0,1},
+        new int[] {1,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,3,1},
+        new int[] {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        new int[] {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+        new int[] {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1},
+        new int[] {1,0,0,0,0,0,0,0,3,0,0,0,0,0,0,1,1,1,1},
+        new int[] {1,0,0,0,0,0,0,1,1,1,1,0,0,1,1,1,1,1,1},
+        new int[] {1,0,0,0,3,0,0,0,0,0,0,0,0,1,1,1,1,1,1},
+        new int[] {1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1},
+        new int[] {1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        new int[] {1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        new int[] {1,0,2,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        new int[] {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+    };
+    
+    [Header("Object Reference")]
+    public Transform wall;
+    public Transform character;
+    //public GameObject character;
+    public Transform orb;
+
+
+    //builds the level
+    void BuildLevel()
+    {
+        //Get the Dynamic Objects and make it the parent 
+        GameObject dynamicParent = GameObject.Find("Dynamic Objects");
+        //Go through each element inside our level variable
+        for(int yPos = 0; yPos < level.Length; yPos++)
+        {
+            for(int xPos = 0; xPos < (level[yPos]).Length; xPos++)
+            {
+                //do nothing if the value is 0
+                //if the value is 1, we want a wall
+                /*if(level[yPos][xPos] == 1)
+                {
+                    //create the wall 
+                    Transform newObject = Instantiate(wall, new Vector3(xPos, (level.Length - yPos), 0), wall.rotation) as Transform;
+                    //set the object's parent to the DynamicObjects variable so it doesn't clutter
+                    newObject.parent = dynamicParent.transform;
+                }*/
+
+                //try switch method
+                Transform toCreate = null;
+                switch(level[yPos][xPos])
+                {
+                    case 0:
+                        //Do nothing 
+                        break;
+                    case 1:
+                        toCreate = wall;
+                        break;
+                    case 2:
+                        toCreate = character;
+                        break;
+                    case 3:
+                        toCreate = orb;
+                        break;
+                    default:
+                        print("Invalid Number: " + (level[yPos][xPos]).ToString());
+                        break;
+                }
+                if(toCreate != null)
+                {
+                    Transform newObject = Instantiate(toCreate, new Vector3(xPos, (level.Length - yPos), 0), toCreate.rotation) as Transform;
+                }
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+       // Instantiate(character, character.transform.position, Quaternion.identity);
+        BuildLevel();
+    }
+}
---
+++ Platformer(untitled)/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    //a reference to our player's rigidbody component
+    private Rigidbody rigidBody;
+    //force to apply when player jumps
+    public Vector2 jumpForce = new Vector2(0, 450);
+    //how fast the player moves along the x-axis
+    public float maxSpeed = 3.0f;
+    //a modifier to the force applied
+    public float speed = 50.0f;
+    //The force to apply that we will get for the player's movement
+    private float xMove;
+    //set to true when the player can jump
+    private bool shouldJump;
+    private bool collidingWall;
+
+    private bool onGround;
+    private float yPrevious;
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + rigidBody.velocity, Color.red);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        shouldJump = false;
+        xMove = 0.0f;
+        onGround = false;
+        yPrevious = Mathf.Floor(transform.position.y);
+        collidingWall = false;
+
+    }
+
+    private void FixedUpdate()
+    {
+        //move the player left and right
+        Movement();
+
+        //sets the camera to center on the player's position
+        //keeping the camera's original depth
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, 
+            Camera.main.transform.position.z);
+    }
+
+    private void Update()
+    {
+        //check if player is Grounded
+        CheckGrounded();
+        Jumping();
+    }
+
+    void CheckGrounded()
+    {
+        //check if the player is hitting something from the center of the object (origin) to slighlty below the bottome of it (distance)
+        float distance = (GetComponent<CapsuleCollider>().height / 2 * this.transform.localScale.y) + .01f;
+        Vector3 floorDirection = transform.TransformDirection(-Vector3.up);
+        Vector3 origin = transform.position;
+
+        if(!onGround)
+        {
+            //Check if there is something directly below us 
+            if (Physics.Raycast(origin, floorDirection, distance))
+            {
+                onGround = true;
+            }
+        }
+        //if we are currently grounded, are we falling down or jumping?
+        else if(Mathf.Floor(transform.position.y) != yPrevious)
+        {
+            onGround = false;
+        }
+        //our current position will be our previous next frame
+        yPrevious = Mathf.Floor(transform.position.y);
+    }
+
+    //If we hit something and we are not grounded, it must be a wall or a ceiling
+    void OnCollisionStay(Collision collision)
+    {
+        if(!onGround)
+        {
+            collidingWall = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        collidingWall = false;
+    }
+
+    void Jumping()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            shouldJump = true;
+        }
+        //if the player should jump
+        if(shouldJump && onGround)
+        {
+            rigidBody.AddForce(jumpForce);
+            shouldJump = false;
+        }
+    }
+
+    void Movement()
+    {
+        //get the player's movement (-1 for left, 1 for right, 0 for none)
+        xMove = Input.GetAxis("Horizontal");
+
+        //if player is touching wall do not apply force
+        if(collidingWall && !onGround)
+        {
+            xMove = 0;
+        }
+
+        if(xMove != 0)
+        {
+            //setting the player horizontal movement
+            float xSpeed = Mathf.Abs(xMove * rigidBody.velocity.x);
+
+            if (xSpeed < maxSpeed)
+            {
+                Vector3 movementForce = new Vector3(1, 0, 0);
+                movementForce *= xMove * speed;
+
+                //RayCast from player to prevent stuck on wall behaviour
+                //SweepTest to check direction traveling and check for colliders within a certain distance
+                //will return true and fill raycast 
+                RaycastHit hit;
+                if (!rigidBody.SweepTest(movementForce, out hit, 0.05f))
+                {
+                    rigidBody.AddForce(movementForce);
+                }
+            }
+            //check speed limit
+            if(Mathf.Abs(rigidBody.velocity.x) > maxSpeed)
+            {
+                Vector2 newVelocity;
+                newVelocity.x = Mathf.Sign(rigidBody.velocity.x) * maxSpeed;
+                newVelocity.y = rigidBody.velocity.y;
+
+                rigidBody.velocity = newVelocity;
+            }
+        }
+        else
+        {
+            //if not moving get slightly slower
+            Vector2 newVelocity = rigidBody.velocity;
+            //reduce current speed by 10%
+            newVelocity.x *= 0.9f;
+            rigidBody.velocity = newVelocity;
+        }
+    }
+}
+
+
